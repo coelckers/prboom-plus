2ce1b9c500a494ab51c7711a72ca093fe64889a2
Fabian Greffrath <fabian@greffrath.com>
2021-04-15 14:33:58 +0200

    support widescreen bunny scroll graphics

--- prboom2/src/f_finale.c
+++ prboom2/src/f_finale.c
@@ -623,15 +623,39 @@ static void F_BunnyScroll (void)
   char        name[10];
   int         stage;
   static int  laststage;
+  static int  p1offset;
+
+  if (finalecount == 0)
+  {
+    const rpatch_t *p1, *p2;
+    p1 = R_CachePatchName(pfub1);
+    p2 = R_CachePatchName(pfub2);
+
+    if (p1->width == 320)
+    {
+      // Unity or original PFUBs.
+      p1offset = (p2->width - 320) / 2;
+    }
+    else
+    {
+      // Widescreen mod PFUBs.
+      p1offset = 0;
+    }
+
+    W_UnlockLumpName(pfub2);
+    W_UnlockLumpName(pfub1);
+  }
 
   {
     int scrolled = 320 - (finalecount-230)/2;
     if (scrolled <= 0) {
       V_DrawNamePatch(0, 0, 0, pfub2, CR_DEFAULT, VPT_STRETCH);
     } else if (scrolled >= 320) {
-      V_DrawNamePatch(0, 0, 0, pfub1, CR_DEFAULT, VPT_STRETCH);
+      V_DrawNamePatch(p1offset, 0, 0, pfub1, CR_DEFAULT, VPT_STRETCH);
+      if (p1offset > 0)
+        V_DrawNamePatch(-320, 0, 0, pfub2, CR_DEFAULT, VPT_STRETCH);
     } else {
-      V_DrawNamePatch(320-scrolled, 0, 0, pfub1, CR_DEFAULT, VPT_STRETCH);
+      V_DrawNamePatch(p1offset+320-scrolled, 0, 0, pfub1, CR_DEFAULT, VPT_STRETCH);
       V_DrawNamePatch(-scrolled, 0, 0, pfub2, CR_DEFAULT, VPT_STRETCH);
     }
   }
