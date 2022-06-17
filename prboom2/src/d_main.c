a83de76ce27cfcb524226a11aad4c8067ea40ff2
feos <vadosnaprimer@users.noreply.github.com>
2022-06-17 13:27:55 +0300

    include wipe effect into video capture (#506)
    * include wipe effect into video capture
    
    fixes #503
    
    * option to disable wipe capture

--- prboom2/src/d_main.c
+++ prboom2/src/d_main.c
@@ -246,6 +246,10 @@ static void D_Wipe(void)
       I_UpdateNoBlit();
       M_Drawer();                   // menu is drawn even on top of wipes
       I_FinishUpdate();             // page flip or blit buffer
+      if (capturing_video && !doSkip && cap_wipescreen)
+      {
+        I_CaptureFrame();
+      }
     }
   while (!done);
 }
