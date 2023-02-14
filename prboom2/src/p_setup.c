0fdd560141003722a18d4070df3d4821cca4d0fb
JadingTsunami <JadingTsunami@users.noreply.github.com>
2023-02-14 07:43:34 -0800

    Fix string overflow with large map numbers. (#569)
    * Fix string overflow with large map numbers.
    
    * Use sizeof() when determining lump size.

--- prboom2/src/p_setup.c
+++ prboom2/src/p_setup.c
@@ -2641,13 +2641,13 @@ void P_SetupLevel(int episode, int map, int playermask, skill_t skill)
   // find map name
   if (gamemode == commercial)
   {
-    sprintf(lumpname, "map%02d", map);           // killough 1/24/98: simplify
-    sprintf(gl_lumpname, "gl_map%02d", map);    // figgi
+    snprintf(lumpname, sizeof(lumpname), "map%02d", map);           // killough 1/24/98: simplify
+    snprintf(gl_lumpname, sizeof(gl_lumpname), "gl_map%02d", map);    // figgi
   }
   else
   {
-    sprintf(lumpname, "E%dM%d", episode, map);   // killough 1/24/98: simplify
-    sprintf(gl_lumpname, "GL_E%iM%i", episode, map); // figgi
+    snprintf(lumpname, sizeof(lumpname), "E%dM%d", episode, map);   // killough 1/24/98: simplify
+    snprintf(gl_lumpname, sizeof(gl_lumpname), "GL_E%iM%i", episode, map); // figgi
   }
 
   lumpnum = W_GetNumForName(lumpname);
