750625d2f31377c026dd859665b1e4f27a214f66
ceski <ceski.dx@gmail.com>
2023-03-29 18:51:01 -0700

    PortMidi: Update volume after "reset all controllers" event

--- prboom2/src/MUSIC/portmidiplayer.c
+++ prboom2/src/MUSIC/portmidiplayer.c
@@ -613,6 +613,7 @@ static void pm_render (void *vdest, unsigned bufflen)
               {
                 writeevent (when, 0xB0, i, 0x7B, 0x00); // all notes off
                 writeevent (when, 0xB0, i, 0x79, 0x00); // reset all controllers
+                write_volume (when, i, DEFAULT_VOLUME); // reset volume
               }
               continue;
             }
@@ -624,6 +625,12 @@ static void pm_render (void *vdest, unsigned bufflen)
         if (currevent->data.channel.param1 == MIDI_CONTROLLER_MAIN_VOLUME)
         {
           write_volume (when, currevent->data.channel.channel, currevent->data.channel.param2);
+          break;
+        }
+        else if (currevent->data.channel.param1 == 0x79)
+        {
+          writeevent (when, 0xB0, currevent->data.channel.channel, 0x79, 0x00); // reset all controllers
+          write_volume (when, currevent->data.channel.channel, DEFAULT_VOLUME); // reset volume
           break;
         }
         // fall through
